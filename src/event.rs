ac0efdbb93a7d916cc307d370a216c5edb53dcff
Shi Lei <shi_lei@massclouds.com>
2024-08-06 08:49:20 +0000

    add getdents64

--- src/event.rs
+++ src/event.rs
@@ -119,6 +119,7 @@ impl TraceEvent {
             SYS_GETPID => self.do_common("getpid", 0),
             SYS_TGKILL => self.do_common("tgkill", 3),
             SYS_WAIT4 => self.do_common("wait4", 4),
+            SYS_GETDENTS64 => self.do_common("getdents64", 3),
             _ => {
                 ("[unknown sysno]", 7, format!("{:#x}", self.result))
             },
@@ -250,12 +251,12 @@ impl TraceEvent {
     }
 
     fn do_write(&self, args: &mut Vec<String>) -> (&'static str, usize, String) {
-        assert_eq!(self.payloads.len(), 1);
-        let payload = &self.payloads.first().unwrap();
-        assert_eq!(payload.inout, crate::OUT);
-        assert_eq!(payload.index, 1);
         args[0] = format!("{}", self.head.ax[0] as isize); // fd
         if self.head.ax[0] == 1 || self.head.ax[0] == 2 {
+            assert_eq!(self.payloads.len(), 1);
+            let payload = &self.payloads.first().unwrap();
+            assert_eq!(payload.inout, crate::OUT);
+            assert_eq!(payload.index, 1);
             args[payload.index] = match CStr::from_bytes_until_nul(&payload.data) {
                 Ok(content) => {
                     format!("{:?}", content)
